cdbc61f2c425e7855b35e7fd2f3b1ddc35eaa554
SashaLoayza <al959@cornell.edu>
2022-10-17 18:10:13 -0400

    Create function stubs for bin/main.ml

--- scrabble_solver/bin/main.ml
+++ scrabble_solver/bin/main.ml
@@ -2,3 +2,19 @@ let () = print_endline "Hello, World!"
 
 
 let valid_words = Arg.read_arg "~/data/dictionary.txt"
+
+(**[validate_letters letters] [Some list], a list of seven chars, or [None] if letters is not a string 
+    of 7 letters, separated by spaces or commas*)
+let validate_letters letters = match letters with 
+| _ -> raise Failure "Unimplemented: validate_letters"
+
+(** [solve letters] returns a list of valid scrabble words given letters. *)
+let solve letters = match validate_letters with
+| _ -> raise Failure "Unimplemented: solve"
+
+
+let main = print_endline "Welcome to team LION's Scrabble Solver!"; 
+print_endline "Enter 7 letters. They may be separated by spaces or commas.";
+match read_line () with
+| exception End_of_file -> ()
+| letters -> solve letters
