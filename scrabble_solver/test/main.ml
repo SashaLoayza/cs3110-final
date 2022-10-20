369c77945c00da69e14de4063f00d5c71e17d7e3
SashaLoayza <al959@cornell.edu>
2022-10-20 11:47:42 -0400

    Change test module name to main.ml and fix test/dune file.

---
+++ scrabble_solver/test/main.ml
@@ -0,0 +1,4 @@
+open OUnit2
+open Scrabble_solver
+open Letter
+open Hand
--- scrabble_solver/test/scrabble_solver.ml
+++
@@ -1,5 +0,0 @@
-(*** open OUnit2 open Letter open Hand
-
-  why you throw error :(
-
-  commented out cause wanted it to compile*)
