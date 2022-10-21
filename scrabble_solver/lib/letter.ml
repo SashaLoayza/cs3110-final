8f09f2673da949089989a18485662a3387ef672a
SashaLoayza <al959@cornell.edu>
2022-10-20 21:40:49 -0400

    Fix Letter.ml to match Letter.mli interface and add blank piece functionality

--- scrabble_solver/lib/letter.ml
+++ scrabble_solver/lib/letter.ml
@@ -1,23 +1,23 @@
 type t = {
-  value : string;
+  value : char;
   points : int;
 }
 
 exception NotValidLetter of string
 
-(** [get_point_val s] is the point value of letter [s]. This is a helper
-    function used by from_input. Requires that [s] is a valid english letter (1
-    character) or the empty string*)
-let get_point_val s =
-  match s with
-  | "A" | "E" | "I" | "O" | "U" | "L" | "N" | "S" | "T" | "R" -> 1
-  | "D" | "G" -> 2
-  | "B" | "C" | "M" | "P" -> 3
-  | "F" | "H" | "V" | "W" | "Y" -> 4
-  | "K" -> 5
-  | "J" | "X" -> 8
-  | "Q" | "Z" -> 10
-  | "" -> 0
+(** [get_point_val s] is the point value of letter [c]. This is a helper
+    function used by from_input. Requires that [c] is a valid english letter (1
+    character) or the underscore, which represents a blank piece.*)
+let get_point_val c =
+  match c with
+  | 'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'S' | 'T' | 'R' -> 1
+  | 'D' | 'G' -> 2
+  | 'B' | 'C' | 'M' | 'P' -> 3
+  | 'F' | 'H' | 'V' | 'W' | 'Y' -> 4
+  | 'K' -> 5
+  | 'J' | 'X' -> 8
+  | 'Q' | 'Z' -> 10
+  | '_' -> 0
   | _ -> raise (NotValidLetter "Not a valid Letter")
 
 let from_input s = { value = s; points = get_point_val s }
