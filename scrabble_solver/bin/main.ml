e9b268b541c645cba43b8cbe2ae7307fbe8c4919
Paul-Garell <prg74@cornell.edu>
2022-10-21 15:07:23 -0400

    Change and Implement specs for validaet_letters
    -Using a string list instead of a char list as the built in library makes it simpler
    -Implemented according to soecifications

--- scrabble_solver/bin/main.ml
+++ scrabble_solver/bin/main.ml
@@ -1,10 +1,16 @@
 let valid_words = Arg.read_arg "~/data/dictionary.txt"
 
-(**[validate_letters letters] [Some list], a list of seven chars, or [None] if
-   letters is not a string of 7 letters, separated by spaces or commas*)
-let validate_letters letters =
-  match letters with
-  | _ -> raise (Failure "Unimplemented: validate_letters")
+(**[validate_letters letters] [string list], a list of seven strings, raises
+   exception if letters is not a string list of length 7 with each inex only
+   having at most length 1*)
+let rec validate_letters letters =
+  if List.length letters = 7 then
+    match letters with
+    | [] -> []
+    | h :: t ->
+        if String.length h < 2 then h :: validate_letters t
+        else raise (Failure "Did not meet preconditions for letter input")
+  else raise (Failure "Did not meet preconditions for letter input")
 
 (** [solve letters] returns a list of valid scrabble words given letters. *)
 let solve letters =
@@ -13,7 +19,7 @@ let solve letters =
 
 let main =
   print_endline "Welcome to team LION's Scrabble Solver!";
-  print_endline "Enter 7 letters. They may be separated by spaces or commas.";
+  print_endline "Enter 7 letters. They may be separated by spaces";
   match read_line () with
   | exception End_of_file -> ()
-  | letters -> solve letters
+  | letters -> solve String.split_on_char ' ' letters
