9100f7ee51a2d4231da370f4b003a1f01fee05a6
Paul-Garell <prg74@cornell.edu>
2022-10-21 19:58:06 -0400

    Updated Validate Letters to Output a Char List Instead of a String List

--- scrabble_solver/bin/main.ml
+++ scrabble_solver/bin/main.ml
@@ -8,18 +8,18 @@ let rec validate_letters letters =
     match letters with
     | [] -> []
     | h :: t ->
-        if String.length h < 2 then h :: validate_letters t
+        if String.length h < 2 then h.[0] :: validate_letters t
         else raise (Failure "Did not meet preconditions for letter input")
   else raise (Failure "Did not meet preconditions for letter input")
 
 (** [solve letters] returns a list of valid scrabble words given letters. *)
 let solve letters =
-  match validate_letters with
+  match validate_letters letters with
   | _ -> raise (Failure "Unimplemented: solve")
 
 let main =
   print_endline "Welcome to team LION's Scrabble Solver!";
-  print_endline "Enter 7 letters. They may be separated by spaces";
+  print_endline "Enter 7 letters. They must be separated by spaces.";
   match read_line () with
   | exception End_of_file -> ()
-  | letters -> solve String.split_on_char ' ' letters
+  | letters -> solve (String.split_on_char ' ' letters)
--- scrabble_solver/lib/hand.ml
+++ scrabble_solver/lib/hand.ml
@@ -1,8 +1,8 @@
-type t = char list
+type t = Letter.t list
 
 (** [from_char_list char_list] returns char_list, which is a valid value of type
     t in the current implementation. t. Requires: char_list is *)
-let from_char_list (char_list : char list) = char_list
+let from_char_list (char_list : string list) = 
 
 let letter_list (t : t) = List.map Letter.from_input t
 let permutations t = raise (Failure "Unimplemented: Hand.permutations")
