39b11854070bdeb2f1f7f3c9424746617ed22be5
Paul-Garell <prg74@cornell.edu>
2022-10-21 20:26:42 -0400

    Update Validate_letters in main.ml for Better Functionality

--- scrabble_solver/bin/main.ml
+++ scrabble_solver/bin/main.ml
@@ -1,21 +1,20 @@
-let valid_words = Arg.read_arg "~/data/dictionary.txt"
+let valid_words = Arg.read_arg "../data/dictionary.txt"
 
 (**[validate_letters letters] [string list], a list of seven strings, raises
    exception if letters is not a string list of length 7 with each inex only
    having at most length 1*)
 let rec validate_letters letters =
-  if List.length letters = 7 then
-    match letters with
-    | [] -> []
-    | h :: t ->
-        if String.length h < 2 then h.[0] :: validate_letters t
-        else raise (Failure "Did not meet preconditions for letter input")
-  else raise (Failure "Did not meet preconditions for letter input")
+  match letters with
+  | [] -> []
+  | h :: t ->
+      if String.length h < 2 then h.[0] :: validate_letters t
+      else raise (Failure "Did not meet preconditions for letter input 1")
 
 (** [solve letters] returns a list of valid scrabble words given letters. *)
 let solve letters =
-  match validate_letters letters with
-  | _ -> raise (Failure "Unimplemented: solve")
+  if List.length letters = 7 && List.length (validate_letters letters) > 1 then
+    ()
+  else raise (Failure "Did not meet preconditions for letter input 2")
 
 let main =
   print_endline "Welcome to team LION's Scrabble Solver!";
