29864fea636e40a420400f2981616f3d22ef0836
Paul-Garell <prg74@cornell.edu>
2022-10-23 15:59:24 -0400

    Added Test Cases For Letter.ml to Get Point Value

--- scrabble_solver/test/main.ml
+++ scrabble_solver/test/main.ml
@@ -9,6 +9,17 @@ let point_val_test (name : string) (c : Letter.t) (expected_output : int) : test
   (* the [printer] tells OUnit how to convert the output to a string *)
   assert_equal expected_output (point_value c)
 
-let letter_tests = [ point_val_test "a is one poiont" (from_input 'a') 1 ]
+let letter_tests =
+  [
+    point_val_test "a is one poiont" (from_input 'a') 1;
+    point_val_test "A is one poiont" (from_input 'A') 1;
+    point_val_test "D is one poiont" (from_input 'D') 2;
+    point_val_test "B is one poiont" (from_input 'B') 3;
+    point_val_test "F is one poiont" (from_input 'F') 4;
+    point_val_test "K is one poiont" (from_input 'K') 5;
+    point_val_test "J is one poiont" (from_input 'J') 8;
+    point_val_test "Q is one poiont" (from_input 'Q') 10;
+  ]
+
 let suite = "test suite for A2" >::: List.flatten [ letter_tests ]
 let _ = run_test_tt_main suite
