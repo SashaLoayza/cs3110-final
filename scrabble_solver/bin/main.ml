9bf1a75b16a32ee3007c9cad1e520d00bf1d9b75
SashaLoayza <al959@cornell.edu>
2022-10-23 17:03:51 -0400

    Change bin/main.ml/main to allow arbitrary number of spaces between input letters

--- scrabble_solver/bin/main.ml
+++ scrabble_solver/bin/main.ml
@@ -5,7 +5,7 @@ open Letter
 let valid_words = Arg.read_arg "data/dictionary.txt"
 
 (**[validate_letters letters] [string list], a list of seven strings, raises
-   exception if letters is not a string list of length 7 with each inex only
+   exception if letters is not a string list of length 7 with each index only
    having at most length 1*)
 let rec validate_letters letters =
   match letters with
@@ -28,4 +28,6 @@ let main =
   print_endline "Enter 7 letters. They must be separated by spaces.";
   match read_line () with
   | exception End_of_file -> ()
-  | letters -> solve (String.split_on_char ' ' letters)
+  | letters ->
+      solve
+        (letters |> String.split_on_char ' ' |> List.filter (fun x -> x <> ""))
