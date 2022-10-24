fc87b2b2df8e951a96a5594f9ba99e82de43eeb8
SashaLoayza <al959@cornell.edu>
2022-10-24 05:33:20 -0400

    Implement permutations in Hand.ml
    - Define and implement function bit_word in lib/Hand.ml that represents
      the bitwise representation of a word.
    - Hand.permutations doesn't include any duplicates.
    - Define and implement function unique.
    - Define and implement unused_letters.
    - Define and implement helper functions:
      - letter_n
      - size
      - duplicates_before
      - char_count
      - bit_n_of_word
      - cons_if_unique
      - cons_if_unused
      - create_perm
      - permutations_n
      - permutations_helper

--- scrabble_solver/lib/hand.ml
+++ scrabble_solver/lib/hand.ml
@@ -1,50 +1,95 @@
 type t = char list
 
-(** [from_char_list char_list] returns char_list, which is a valid value of type
-    t in the current implementation. t. Requires: char_list is *)
-let from_char_list (char_list : char list) : t = char_list
+let from_char_list (char_list : char list) : t =
+  List.sort Char.compare (List.map Char.uppercase_ascii char_list)
 
-let letter_list (t : t) = List.map Letter.from_input t
-let to_string_list t = List.map (String.make 1) t
+let char_list t = t
+let size t = List.length (char_list t)
+let letter_list (t : t) = List.map Letter.from_input (char_list t)
+let to_string_list t = List.map (String.make 1) (char_list t)
 
-(**[factorial n] is n!. Requires: n>1.*)
-let factorial n =
-  let rec factorial_tr n acc =
-    if n = 0 then acc else factorial_tr (n - 1) (acc * n)
+(** [letter_n t n] is the nth letter of [Hand.t t]*)
+let letter_n t n = List.nth (from_char_list t) n
+
+(** duplicates_before t n] is the number of letters before the nth letter in [Hand.t t] that are equivalent to [letter_n t n]. Requires: n>=0. *)
+let duplicates_before t n =
+  let l = letter_n t n in
+  let rec duplicates_helper t n acc =
+    if n = 0 || letter_n t (n - 1) <> l then acc
+    else duplicates_helper t (n - 1) (acc + 1)
   in
-  factorial_tr n 1
+  duplicates_helper t n 0
 
-(**[perm_number n t] is the number of possible words made by the letters in
-   [hand.t t]*)
-let perm_number n t = factorial (List.length (letter_list t)) / factorial n
+(**[char_count c  s] is the number of occurrences of c in s.*)
+let char_count c s =
+  let rec char_count_helper (c : char) (s : string) (acc : int) =
+    match s with
+    | "" -> acc
+    | s ->
+        if not (String.contains s c) then acc
+        else if s.[0] = c then
+          char_count_helper c (String.sub s 1 (String.length s - 1)) (acc + 1)
+        else char_count_helper c (String.sub s 1 (String.length s - 1)) acc
+  in
+  char_count_helper c s 0
 
-(** [unused_letters t perm] is the [char list] of the unused letters of *)
-let unused_letters t perm =
-  let result_array = Array.of_list t in
-  (*result will be mutated to only include the unused letters of perm. The used
-    letters will be changed to the char '0'.*)
-  for n = 0 to Array.length result_array do
-    if String.contains perm result_array.(n) then result_array.(n) <- '0'
-    else ()
-  done;
-  List.filter (fun c -> c = '0') (Array.to_list result_array)
+(**[bit_n_of_word t s acc index] is the truth value of the [n]th bit in the
+   bit-vector representation of string s. Requires: [s] is a string made up of
+   only letters from [Hand.t t], that acc[0..n-1] is the correct partial
+   bit-vector representation of word s, and that the value of [acc] at [index]
+   is false. *)
+let bit_n_of_word (t : t) (s : string) (acc : Bitv.t) (index : int) =
+  let l = letter_n t index in
+  if not (String.contains s l) then ()
+    (*keep the bit false if the string does not contain the relevant letter*)
+  else if char_count l s > duplicates_before t index then
+    Bitv.set acc index true
+    (*the letter is contained in s when there are more instances of the letter
+      in the string than duplicates of the letter before position n in hand.*)
+  else ()
 
+let bit_word (s : string) (t : t) =
+  let result = Bitv.create (size t) false in
+  Bitv.iteri (fun index _ -> bit_n_of_word t s result index) result;
+  result
+
+let cons_if_unique acc elt = if List.mem elt acc then acc else elt :: acc
+let unique a_list = List.fold_left cons_if_unique [] a_list
+
+(** unused_letters t perm is a char list of the unique set of letters that are
+    in [Hand.t t] but not in perm. Requires: All letters in perm are from
+    [Hand.t t] and there are no more letters of any character than t has
+    available. *)
+
+let cons_if_unused t acc index elt =
+  if elt then acc (*elt is true so [letter_n t index] is used*)
+  else letter_n t index :: acc
+
+let unused_letters (t : t) perm =
+  bit_word perm t
+  |> Bitv.foldi_left (fun acc index elt -> cons_if_unused t acc index elt) []
+  |> unique
+
+(**[create_perm perm_index perm t] is the string with the perm_index^th unused
+   letter added to the end of it.*)
 let create_perm perm_index perm t =
   let unused = unused_letters t perm in
   perm ^ String.make 1 (List.nth unused perm_index)
 
-(**[permutations_n n t] is the list of all length n strings made by combining
-   letters in [hand.t t ]. In the current implementation, there may be duplicate
-   words if there are duplicate letters in [hand.t t]*)
-let permutations_n n t (previous : string list) =
+(**[permutations_n t n] is the list of all length n strings made by combining
+   letters in [Hand.t t ]. In the current implementation, there are no duplicate
+   permutations returned. *)
+let permutations_n t n (previous : string list) =
   let string_list = to_string_list t in
-  let extra_letters = List.length string_list - n in
   match previous with
-  | [] -> string_list
+  | [] -> unique string_list
   | _ ->
       let result =
         Array.init (List.length previous) (fun prev_index ->
-            Array.make extra_letters (List.nth previous prev_index))
+            let unused = unused_letters t (List.nth previous prev_index) in
+            Array.make (List.length unused) (List.nth previous prev_index))
+        (*Only create one new copy of the previous n string for each unused
+          letter*)
       in
       Array.iteri
         (fun prev_index perm_array ->
@@ -58,12 +103,12 @@ let permutations_n n t (previous : string list) =
 let permutations_helper (t : t) (current_result : string list array) n list_n =
   match n with
   | 0 -> ()
-  | _ -> current_result.(n) <- permutations_n n t current_result.(n - 1)
+  | _ -> current_result.(n) <- permutations_n t n current_result.(n - 1)
 
-let rec permutations t =
-  let result = Array.make 7 [] in
+let permutations t =
+  let result = Array.make (size t + 1) [] in
   Array.iteri (permutations_helper t result) result;
   result |> Array.to_list |> List.concat
 
 let word_list dictionary t =
-  List.filter (fun perms -> Array.mem perms dictionary) (permutations t)
+  List.filter (fun perm -> Array.mem perm dictionary) (permutations t)
--- scrabble_solver/lib/hand.mli
+++ scrabble_solver/lib/hand.mli
@@ -1,13 +1,39 @@
-(** A player's hand*)
-
 type t
+(** A player's hand, with letters sorted in alphabetical order.*)
 
 val from_char_list : char list -> t
 (** [from_char_list char_list] is the representation of the players hand as a
-    value of type t.*)
+    value of type [Hand.t]. It sorts the input char list so that t is sorted.*)
 
+val char_list : t -> char list
+(** [char_list char_list] is the representation of the players hand as a value
+    of type [char list], where all chars are in uppercase ascii representation.*)
+
+val size : t -> int
+(**[size t] is the number of letters in [Hand.t t]*)
 val letter_list : t -> Letter.t list
 (** [letter_list t] is a [Letter.t list] of the letters in [t:hand]*)
+
+val unique : char list -> char list
+(** [unique chars] is the char list of the characters in chars with duplicates
+    removed*)
+
+val bit_word : string -> t -> Bitv.t
+(**bit_hand is the (7-bit) bitwise [Bitv.t] representation of [string s] with
+   respect to the initial ordering of [Hand.t t], which is maintained in
+   [char_list t]. If there are duplicate letters in hand, then they are
+   populated left to right in the bitwise representation of the integer.For
+   example, if [char_list t] is ['a', 'b', 'b', 'd', 'e', 'x', 'y', ], then
+   [bit_word "bad"] is the int 1101000. *)
+
+val unused_letters : t -> string -> char list
+(** [unused_letters t perm] is the [char list] of the unused letters of
+    [Hand.t t] in string [perm].*)
+
+val permutations : t -> string list
+(** [permutations t] is the [string list] of all possible permutations of the
+    letters in [Hand.t t]. In the current implementation, this includes
+    duplicates if [t] has duplicate letters.*)
 
 val word_list : string array -> t -> string list
 (** [word_list dictionary t] is a list of valid words that can be made out of
