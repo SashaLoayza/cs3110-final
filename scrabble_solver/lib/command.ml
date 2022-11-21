7d051c2a31525a8c1b150f9dad34bcdc4a96f890
SashaLoayza <al959@cornell.edu>
2022-11-20 23:09:37 -0500

    Add Command compilation unit

---
+++ scrabble_solver/lib/command.ml
@@ -0,0 +1,29 @@
+type t =
+  | BoardAdd of {pos: int * int; x : Letter.t} (*Add a single tile to the board*)
+  | BoardAddWord of {pos: int*int; direction : Board.direction; length : int}(*Add a word/succession of tiles to the board*)
+  | BoardRem of int * int (*Remove a single tile from the board*)
+  | BoardRemWord of {pos: int*int; direction : Board.direction; length : int}(*Remove a word/succession of tiles from the board*)
+  | BoardClear (*Clear the board*)
+  | HandMake of Letter.t list
+    (* Clear the current hand, if any, and initialize a new hand of up to 7
+       letters. *)
+  | HandAdd of Letter.t
+    (* Add a single letter to the current hand, if it has less than 7
+       letters. *)
+  | HandRem of Letter.t (* Remove a single letter from the current hand*)
+  | Undo (* Undo the last command*)
+  | Solve (* Run the solver engine and print the top solutions.*)
+  | Exit  (* Exit the program*)
+
+exception EmptyCommand
+(**[parse_cmd cmd_string] is a list of strings, where the first string is the
+   command passed in, and all subsequent strings are the arguments of the
+   command (if any are given).Raises: an EmptyCommand exception, when the resulting list is empty. *)
+let parse_cmd cmd_string : string list =
+  let result = 
+  List.filter (fun x -> x <> " ") (String.split_on_char ' ' cmd_string)in
+  match result with 
+  |[] -> raise EmptyCommand
+  | _ -> result
+
+let cmd_of_string string = raise (Failure "Unimplemented")
---
+++ scrabble_solver/lib/command.mli
@@ -0,0 +1,5 @@
+type t
+(**The type of a command*)
+
+val cmd_of_string : string -> t
+(**Given an input string, return the correct command of type t*)
