ec5d80cd64a9078084e51ffb9bc5d8689bc31d36
SashaLoayza <al959@cornell.edu>
2022-12-08 15:54:12 -0500

    Add prev_command to all states

--- scrabble_solver/lib/state.ml
+++ scrabble_solver/lib/state.ml
@@ -1,23 +1,29 @@
 type t =
   (*state includes the last command*)
-  | Initialize of { board : Board.t }
+  | Initialize of {
+      board : Board.t;
+      prev_command : Command.t;
+    }
     (*State when player is creating the board (populating it with letters/words
       for the first time.)*)
   | MakeHand of {
       hand : Hand.t;
       board : Board.t;
+      prev_command : Command.t;
     }
     (*State when player is creating or changing the letters in their hand after
       initializing the board.*)
   | UpdateBoard of {
       hand : Hand.t;
       board : Board.t;
+      prev_command : Command.t;
     }
     (*State when player is updating board after initializing the board and
       setting their hand.*)
   | Solve of {
       hand : Hand.t;
       board : Board.t;
+      prev_command : Command.t;
     }
 (*State when player is creating or changing the letters in their hand.*)
 
