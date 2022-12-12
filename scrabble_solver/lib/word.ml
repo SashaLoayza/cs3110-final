405e19c760028d1262ad2b075b8636b21e3dca6b
SashaLoayza <al959@cornell.edu>
2022-12-12 16:58:35 -0500

    Remove opens from bin/main and implement Word.from_input

--- scrabble_solver/lib/word.ml
+++ scrabble_solver/lib/word.ml
@@ -8,3 +8,15 @@ type t = {
   length : int;
   letter_list : Letter.t option list;
 }
+
+(*Take in string [word] and convert to a char list.*)
+let string_to_char_list (word : string) : char list =
+  List.init (String.length word) (fun n ->
+      String.get word n |> Char.uppercase_ascii)
+
+(*Create a word from input.*)
+let from_input (pos : int * int) (direction : direction) (word : string) =
+  let letter_list =
+    word |> string_to_char_list |> List.map Letter.from_input_opt
+  in
+  { pos; direction; length = String.length word; letter_list }
--- scrabble_solver/test/main.ml
+++ scrabble_solver/test/main.ml
@@ -1,26 +1,22 @@
 open OUnit2
 open Scrabble_solver
-open Hand
-open Letter
-open Dictionary
-open Board
 
 let point_val_test (name : string) (c : Letter.t) (expected_output : int) : test
     =
   name >:: fun _ ->
   (* the [printer] tells OUnit how to convert the output to a string *)
-  assert_equal expected_output (point_value c)
+  assert_equal expected_output (Letter.point_value c)
 
 let letter_tests =
   [
-    point_val_test "a is one point" (from_input 'a') 1;
-    point_val_test "A is one point" (from_input 'A') 1;
-    point_val_test "D is two points" (from_input 'D') 2;
-    point_val_test "B is three points" (from_input 'B') 3;
-    point_val_test "F is four points" (from_input 'F') 4;
-    point_val_test "K is five points" (from_input 'K') 5;
-    point_val_test "J is eight points" (from_input 'J') 8;
-    point_val_test "Q is ten points" (from_input 'Q') 10;
+    point_val_test "a is one point" (Letter.from_input 'a') 1;
+    point_val_test "A is one point" (Letter.from_input 'A') 1;
+    point_val_test "D is two points" (Letter.from_input 'D') 2;
+    point_val_test "B is three points" (Letter.from_input 'B') 3;
+    point_val_test "F is four points" (Letter.from_input 'F') 4;
+    point_val_test "K is five points" (Letter.from_input 'K') 5;
+    point_val_test "J is eight points" (Letter.from_input 'J') 8;
+    point_val_test "Q is ten points" (Letter.from_input 'Q') 10;
   ]
 
 (*[get_words_test] tests both create_hash and find_words as the respective
@@ -31,21 +27,21 @@ let get_words_test (name : string) (t : Dictionary.t) (l : string)
     (expected_output : string list) : test =
   name >:: fun _ ->
   (* the [printer] tells OUnit how to convert the output to a string *)
-  assert_equal expected_output (get_words t l)
+  assert_equal expected_output (Dictionary.get_words t l)
 
 let dictionary_tests =
   [
     get_words_test "1 key value pair"
-      (create_hash [| "oneWord" |])
+      (Dictionary.create_hash [| "oneWord" |])
       "oneWord" [ "oneWord" ];
     get_words_test "1 key, 2 values find based  on first word"
-      (create_hash [| "oneWord"; "Wordone" |])
+      (Dictionary.create_hash [| "oneWord"; "Wordone" |])
       "oneWord" [ "Wordone"; "oneWord" ];
     get_words_test "1 key, 2 values, find based on second word"
-      (create_hash [| "oneWord"; "Wordone" |])
+      (Dictionary.create_hash [| "oneWord"; "Wordone" |])
       "Wordone" [ "Wordone"; "oneWord" ];
     get_words_test "2 words, but find key is not present"
-      (create_hash [| "oneWord"; "Wordone" |])
+      (Dictionary.create_hash [| "oneWord"; "Wordone" |])
       "no" [];
   ]
 
@@ -53,11 +49,11 @@ let hand_tests = []
 
 let add_word_test (name : string) (board : Board.t) (word : Word.t)
     (expected_output : Board.t) : test =
-  name >:: fun _ -> assert_equal expected_output (add_word board word)
+  name >:: fun _ -> assert_equal expected_output (Board.add_word board word)
 
-let letterc = from_input 'c'
-let lettera = from_input 'a'
-let lettert = from_input 't'
+let letterc = Letter.from_input 'c'
+let lettera = Letter.from_input 'a'
+let lettert = Letter.from_input 't'
 let board_tests = []
 let main_tests = []
 
