ed53f1753c471a36a26d70a765077f0696fa37f7
Ian Yang <ijy2@cornell.edu>
2022-12-12 23:11:48 -0500

    Removed remove from board

--- scrabble_solver/lib/board.mli
+++ scrabble_solver/lib/board.mli
@@ -18,11 +18,6 @@ val place : t -> Letter.t option -> int -> int -> t
     [board\[row\]\[column\]]. If a letter is already on that tile, that letter
     is replaced by [letter].*)
 
-val remove : t -> int -> int -> t
-(** [remove board row column] is the board with the letter at position
-    [board\[row\]\[column\]] removed. If there is no letter at that position,
-    the same board is returned.*)
-
 val add_word : t -> Word.t -> t
 val row_to_string : t -> int -> string
 val col_to_string : t -> int -> string
