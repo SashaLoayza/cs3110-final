f43fc0d7de9a735d655288dcc621856e83d25f70
SashaLoayza <al959@cornell.edu>
2022-12-13 17:26:49 -0500

    Update state.mli and word.ml specifications

--- scrabble_solver/lib/state.mli
+++ scrabble_solver/lib/state.mli
@@ -4,6 +4,7 @@ type t
     environment (i.e. solve), and the changes made by the command*)
 
 val initial_state : t
+(** The state of the program before any user input has occured.*)
 
 val execute_cmd : t -> Command.t -> Dictionary.t -> t
 (** The new state after the given command has been executed.*)
--- scrabble_solver/lib/word.ml
+++ scrabble_solver/lib/word.ml
@@ -1,3 +1,5 @@
+(** The direction of a word on the board, (only right and down are valid
+    directions in scrabble)*)
 type direction =
   | Right
   | Down
@@ -8,13 +10,15 @@ type t = {
   length : int;
   letter_list : Letter.t option list;
 }
+(**A word that is to be placed on the board, where the letter_list is None at
+   points where the player skips over an existing tile on the board. *)
 
-(*Take in string [word] and convert to a char list.*)
+(**Take in string [word] and convert to a char list.*)
 let string_to_char_list (word : string) : char list =
   List.init (String.length word) (fun n ->
       String.get word n |> Char.uppercase_ascii)
 
-(*Create a word from input.*)
+(**Create a Word.t value from user input.*)
 let from_input (pos : int * int) (direction : direction) (word : string) =
   let letter_list =
     word |> string_to_char_list |> List.map Letter.from_input_opt
